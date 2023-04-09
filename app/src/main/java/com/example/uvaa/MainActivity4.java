57b2566c1556ce15fe1b371de9319ee982ed842d
vedika962 <vedikakulkarni962@gmail.com>
2023-04-09 19:04:40 +0530

    new added

--- app/src/main/java/com/example/uvaa/MainActivity4.java
+++ app/src/main/java/com/example/uvaa/MainActivity4.java
@@ -1,5 +1,6 @@
 package com.example.uvaa;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
@@ -9,37 +10,58 @@ import android.widget.TextView;
 import android.text.TextWatcher;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
-public class MainActivity4 extends AppCompatActivity {
+public class MainActivity4 extends AppCompatActivity implements View.OnClickListener {
 
+    public CardView c1, c2, c3, c4, c5;
 
+    @SuppressLint("MissingInflatedId")
     @Override
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main4);
+        c1 = (CardView) findViewById(R.id.card1);
+        c2 = (CardView) findViewById(R.id.card2);
+        c3 = (CardView) findViewById(R.id.card3);
+        c4 = (CardView) findViewById(R.id.card4);
+        c5 = (CardView) findViewById(R.id.card5);
+        c1.setOnClickListener(this);
+        c2.setOnClickListener(this);
+        c3.setOnClickListener(this);
+        c4.setOnClickListener(this);
+        c5.setOnClickListener(this);
 
 
     }
-    public void put(View v)
-    {
-        Intent i=new Intent(this,MainActivity2.class);
-        startActivity(i);
-    }
-    public void get(View v)
-    {
-        Intent i=new Intent(this,MainActivity5.class);
-        startActivity(i);
-    }
-    public void getDept(View v)
-    {
-        Intent i=new Intent(this,MainActivity8.class);
-        startActivity(i);
-    }
-    public void vPanel(View v)
-    {
-        Intent i=new Intent(this,MainActivity6.class);
-        startActivity(i);
-    }
 
+
+    @Override
+    public void onClick(View view) {
+        Intent i;
+        switch (view.getId()) {
+            case R.id.card1:
+                i = new Intent(this, MainActivity2.class);
+                startActivity(i);
+                break;
+            /*case R.id.card2:
+                i=new Intent(this,MainActivity2.class);
+                startActivity(i);
+                break;*/
+            case R.id.card3:
+                i = new Intent(this, MainActivity8.class);
+                startActivity(i);
+                break;
+            case R.id.card4:
+                i = new Intent(this, MainActivity7.class);
+                startActivity(i);
+                break;
+            case R.id.card5:
+                i = new Intent(this, MainActivity6.class);
+                startActivity(i);
+                break;
+
+        }
+    }
 }
--- app/src/main/java/com/example/uvaa/MainActivity6.java
+++ app/src/main/java/com/example/uvaa/MainActivity6.java
@@ -5,14 +5,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity6 extends AppCompatActivity {
     Button b;
     TextView t1,t2,t3,t4;
     Spinner s1,s2,s3,s4;
+    String[] num={"1","2","3","4"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,6 +29,12 @@ public class MainActivity6 extends AppCompatActivity {
         s2=findViewById(R.id.spinner5);
         s3=findViewById(R.id.spinner6);
         s4=findViewById(R.id.spinner7);
+
+        ArrayAdapter<String> adapter=new ArrayAdapter<String>(MainActivity6.this,android.R.layout.simple_spinner_item,num);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        s2.setAdapter(adapter);
+        s3.setAdapter(adapter);
 
     }
     public void display(View v)
@@ -44,5 +53,9 @@ public class MainActivity6 extends AppCompatActivity {
         s2.setVisibility(View.VISIBLE);
         s3.setVisibility(View.VISIBLE);
         s4.setVisibility(View.VISIBLE);
+    }
+    public void click(View v)
+    {
+        Toast.makeText(this, "Make new entry again ", Toast.LENGTH_SHORT).show();
     }
 }
