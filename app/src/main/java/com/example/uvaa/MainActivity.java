5a32d8d055b662771467af984f19213f83908dae
Aniket775 <aniketwarule775@gmail.com>
2023-04-10 01:26:18 +0530

    Database helper added

--- app/src/main/java/com/example/uvaa/MainActivity.java
+++ app/src/main/java/com/example/uvaa/MainActivity.java
@@ -3,7 +3,9 @@ package com.example.uvaa;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,6 +14,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SharedPreferences st = PreferenceManager.getDefaultSharedPreferences(this);
+        if(!st.getBoolean("firstTime",false))
+        {
+            //Runs only one time
+            MyDbHelper mdh = new MyDbHelper(this);
+            mdh.addadmin("itadmin","itadmin");
+            mdh.addadmin("coadmin","coadmin");
+            mdh.addadmin("mechadmin","mechadmin");
+            mdh.addadmin("civiladmin","civiladmin");
+            mdh.addadmin("elecadmin","alecadmin");
+            mdh.addadmin("entcadmin","antcadmin");
+            mdh.addadmin("aeadmin","aeadmin");
+            mdh.addadmin("ddgmadmin","ddgmadmin");
+            mdh.addadmin("aimladmin","aimladmin");
+
+            SharedPreferences.Editor editor = st.edit();
+            editor.putBoolean("firstTime",true);
+            editor.commit();
+        }
+
     }
     public void activity(View v)
     {
