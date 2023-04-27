2803f0b6c0bacbe5465f637c682e528ef7e278a6
vedika962 <vedikakulkarni962@gmail.com>
2023-04-27 18:30:38 +0530

    new added

--- app/src/main/java/com/example/uvaa/MainActivity6.java
+++ app/src/main/java/com/example/uvaa/MainActivity6.java
@@ -13,8 +13,8 @@ import android.widget.Toast;
 
 public class MainActivity6 extends AppCompatActivity {
     Button b;
-    TextView t1,t2,t3,t4;
-    Spinner s1,s2,s3,s4;
+    TextView t1,t3,t4;
+    Spinner s2,s3,s4;
     String[] num={"1","2","3","4"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,10 +22,10 @@ public class MainActivity6 extends AppCompatActivity {
         setContentView(R.layout.activity_main6);
         b=findViewById(R.id.button15);
         t1=findViewById(R.id.textView29);
-        t2=findViewById(R.id.textView30);
+
         t3=findViewById(R.id.textView32);
         t4=findViewById(R.id.textView33);
-        s1=findViewById(R.id.spinner4);
+
         s2=findViewById(R.id.spinner5);
         s3=findViewById(R.id.spinner6);
         s4=findViewById(R.id.spinner7);
@@ -46,10 +46,10 @@ public class MainActivity6 extends AppCompatActivity {
     {
         b.setVisibility(View.VISIBLE);
         t1.setVisibility(View.VISIBLE);
-        t2.setVisibility(View.VISIBLE);
+
         t3.setVisibility(View.VISIBLE);
         t4.setVisibility(View.VISIBLE);
-        s1.setVisibility(View.VISIBLE);
+
         s2.setVisibility(View.VISIBLE);
         s3.setVisibility(View.VISIBLE);
         s4.setVisibility(View.VISIBLE);
