738d60b967a974675fb36f79ca3199246b158dfb
vedika962 <vedikakulkarni962@gmail.com>
2023-04-28 12:08:08 +0530

    28-04-23 12:07

--- app/src/main/java/com/example/uvaa/MainActivity2.java
+++ app/src/main/java/com/example/uvaa/MainActivity2.java
@@ -1,31 +1,31 @@
 package com.example.uvaa;
 
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.annotation.SuppressLint;
 import android.app.Dialog;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Spinner;
 
 public class MainActivity2 extends AppCompatActivity {
 
-    EditText vl_name,vl_mob,vl_user,vl_pass;
+    EditText vl_name,vl_mob,vl_user,vl_pass,sub_name,sub_yr,sub_lect,sub_prac;
     Dialog dilog;
-    Button b1;
+    Button b1,b3;
+    String[][] v=new String[100][4];
+    int a=0;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
 
-        vl_name = (EditText) findViewById(R.id.editTextTextPersonName3);
-        vl_mob = (EditText) findViewById(R.id.editTextTextPersonName4);
-        vl_user = (EditText) findViewById(R.id.editTextTextPersonName5);
-        vl_pass = (EditText) findViewById(R.id.editTextTextPassword);
+        vl_name = (EditText)findViewById(R.id.editTextTextPersonName3);
+        vl_mob = (EditText)findViewById(R.id.editTextTextPersonName4);
+        vl_user = (EditText)findViewById(R.id.editTextTextPersonName5);
+        vl_pass = (EditText)findViewById(R.id.editTextTextPassword);
 
         dilog = new Dialog(MainActivity2.this);
         dilog.setContentView(R.layout.custom_dialog);
@@ -39,6 +39,26 @@ public class MainActivity2 extends AppCompatActivity {
            {
                dilog.show();
            }
+        });
+        b3=dilog.findViewById(R.id.button8);
+        b3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dilog.hide();
+                sub_name=(EditText)dilog.findViewById(R.id.editTextTextPersonName16);
+                sub_yr=(EditText) dilog.findViewById(R.id.editTextTextPersonName);
+                sub_lect=(EditText) dilog.findViewById(R.id.editTextTextPersonName17);
+                sub_prac=(EditText) dilog.findViewById(R.id.editTextTextPersonName18);
+                String subname=sub_name.getText().toString();
+                String subyr=sub_yr.getText().toString();
+                String sublect=sub_lect.getText().toString();
+                String subprac=sub_prac.getText().toString();
+                for(int i=0;i<=4;i++)
+                {
+                    v[i][a]=subname;
+                }
+
+            }
         });
 
 
