4566167e0aaa36ca788d30e75331244d5834c476
vedika962 <vedikakulkarni962@gmail.com>
2023-05-02 15:17:34 +0530

    2-05-23 3:23

--- app/src/main/java/com/example/uvaa/MainActivity6.java
+++ app/src/main/java/com/example/uvaa/MainActivity6.java
@@ -15,9 +15,11 @@ import android.widget.Toast;
 
 public class MainActivity6 extends AppCompatActivity {
     Button b,b1,b2;
-    TextView t1,t3,t4,t5;
-    Spinner s2,s3,s4;
+    TextView t1,t3,t4,t5,t6,t7;
+    Spinner s2,s3,s4,s5,s6;
     String[] num={"1","2","3","4"};
+    String[] slot={"10:30-11:30","11:30-12:30","12:30-1:30","2:00-3:00","3:00-4:00","4:15-5:15","5:15-6:15"};
+    String[] slot1={"10:30-12:30","11:30-1:30","2:00-4:00","4:15-6:15"};
     Dialog d;
     EditText txt1,txt2,txt3;
     MyDbHelper db = new MyDbHelper(this);
@@ -37,10 +39,15 @@ public class MainActivity6 extends AppCompatActivity {
         t3=findViewById(R.id.textView32);
         t4=findViewById(R.id.textView33);
         t5=findViewById(R.id.textView37);
+        t6=findViewById(R.id.textView);
+        t7=findViewById(R.id.textView42);
 
         s2=findViewById(R.id.spinner5);
         s3=findViewById(R.id.spinner6);
         s4=findViewById(R.id.spinner7);
+        s5=findViewById(R.id.spinner4);
+        s6=findViewById(R.id.spinner8);
+
         txt3=findViewById(R.id.editTextTextPersonName20);
 
         ArrayAdapter<String> adapter=new ArrayAdapter<String>(MainActivity6.this,android.R.layout.simple_spinner_item,num);
@@ -48,6 +55,17 @@ public class MainActivity6 extends AppCompatActivity {
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         s2.setAdapter(adapter);
         s3.setAdapter(adapter);
+
+        ArrayAdapter<String> a=new ArrayAdapter<String>(MainActivity6.this,android.R.layout.simple_spinner_item,slot);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        s5.setAdapter(a);
+
+        ArrayAdapter<String> ad=new ArrayAdapter<String>(MainActivity6.this,android.R.layout.simple_spinner_item,slot1);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        s6.setAdapter(ad);
+
         d= new Dialog(MainActivity6.this);
         d.setContentView(R.layout.custom_dilog2);
         d.setCancelable(false);
@@ -107,6 +125,8 @@ public class MainActivity6 extends AppCompatActivity {
         t3.setVisibility(View.VISIBLE);
         t4.setVisibility(View.VISIBLE);
         t5.setVisibility(View.VISIBLE);
+        t6.setVisibility(View.VISIBLE);
+        t7.setVisibility(View.VISIBLE);
         txt3.setVisibility(View.VISIBLE);
 
         s2.setVisibility(View.VISIBLE);
