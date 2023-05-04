708ef322432690d5ab2e7e3597e1a5598ba3d5be
vedika962 <vedikakulkarni962@gmail.com>
2023-05-04 18:22:20 +0530

    4-05-23 6:00

--- app/src/main/java/com/example/uvaa/MainActivity8.java
+++ app/src/main/java/com/example/uvaa/MainActivity8.java
@@ -9,16 +9,17 @@ import android.widget.Spinner;
 public class MainActivity8 extends AppCompatActivity {
 
     Spinner s1,s2;
-    String[] dept={"IT","CO","ET","EE","ME","CE","DDGM","AE"};
+    String[] year={"2022-23","2023-24","2024-25","2025-26","2026-27","2027-28","2028-29","2029-30"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main8);
         s1 =findViewById(R.id.spinner);
-        ArrayAdapter<String> adapter=new ArrayAdapter<String>(MainActivity8.this,android.R.layout.simple_spinner_item,dept);
 
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        s1.setAdapter(adapter);
+
+        ArrayAdapter<String> adapter1=new ArrayAdapter<String>(MainActivity8.this,android.R.layout.simple_spinner_item,year);
+        adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        s1.setAdapter(adapter1);
 
     }
 }
